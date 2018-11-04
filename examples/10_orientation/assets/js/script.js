4f60000fbd702df5eef8250f664d05ce8fc2ddc3
chris hamamoto <chamamoto@cca.edu>
2018-11-04 14:28:48 -0800

    Update examples/10_orientation/assets/js/script.js

--- examples/10_orientation/assets/js/script.js
+++ examples/10_orientation/assets/js/script.js
@@ -1,35 +1,45 @@
 $(document).ready(function(){
 
-if(window.DeviceMotionEvent){
-  window.addEventListener("devicemotion", motion, false);
-}else{
-  console.log("DeviceMotionEvent is not supported");
-}
+	if(window.DeviceMotionEvent){
+	  window.addEventListener("devicemotion", motion, false);
+	}else{
+	  console.log("DeviceMotionEvent is not supported");
+	}
 
-function motion(event){
-  $('#accel').html("Accelerometer: "
-    + event.accelerationIncludingGravity.x + ", "
-    + event.accelerationIncludingGravity.y + ", "
-    + event.accelerationIncludingGravity.z
-  );
-}
+	function motion(event){
+	  $('#accel').html("Accelerometer: "
+	    + event.accelerationIncludingGravity.x + ", "
+	    + event.accelerationIncludingGravity.y + ", "
+	    + event.accelerationIncludingGravity.z
+	  );
+	}
 
 
-if(window.DeviceOrientationEvent){
-  window.addEventListener("deviceorientation", orientation, false);
-}else{
-  console.log("DeviceOrientationEvent is not supported");
-}
+	if(window.DeviceOrientationEvent){
+	  window.addEventListener("deviceorientation", orientation, false);
+	}else{
+	  console.log("DeviceOrientationEvent is not supported");
+	}
 
-function orientation(event){
-  $('#magnet').html("Magnetometer: "
-    + event.alpha + ", "
-    + event.beta + ", "
-    + event.gamma
-  );
-}
+	var alpha;
+	var beta;
+	var gamma;
 
-$('body').css('background', 'rgb('+math.floor(event.alpha)+', '+math.floor(event.beta)+', '+math.floor(event.gamma)+')')
+	function orientation(event){
+	  $('#magnet').html("Magnetometer: "
+	    + event.alpha + ", "
+	    + event.beta + ", "
+	    + event.gamma
+	  );
+
+	  alpha =  Math.floor(event.alpha);
+	  beta =  Math.floor(event.beta);
+	  gamma =  Math.floor(event.gamma);
+
+	  $('body').css('background-color', 'rgb('+alpha+', '+beta+', '+gamma+')');
+
+	}
+
 
 
 });
